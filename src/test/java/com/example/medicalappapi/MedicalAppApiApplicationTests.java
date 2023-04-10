2e11476963ac7894a1b0b4705a99d45b03f13ddb
MarioBujor2001 <bujormario2001@gmail.com>
2023-04-10 17:25:13 +0300

    second version of the api, works with android

---
+++ src/test/java/com/example/medicalappapi/MedicalAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicalappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
